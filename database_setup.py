393d9809967c1977992b225be731f72ae120bebb
AsemDevs <asemhamdi97@gmail.com>
2019-09-24 18:21:12 +0100

    Adding tables to the database

--- database_setup.py
+++ database_setup.py
@@ -14,4 +14,19 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
+class City(Base):
+    __tablename__ = 'city'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Place(Base):
+    __tablename__ = 'place'
+
+	name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    city_id = Column(Integer,ForeignKey('city.id'))
+    city = relationship(City) 
+
 Base.metadata.create_all(engine)
