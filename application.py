1d8e1886386794c1c63e0744074463f2e72a7b7d
AsemDevs <asemhamdi97@gmail.com>
2019-09-24 18:13:34 +0100

    Adding Routes

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem, User
+from database_setup import User
 from flask import session as login_session
 import random
 import string
@@ -14,19 +14,46 @@ import requests
 
 app = Flask(__name__)
 engine = create_engine('sqlite:///citiescatalog.db')
-Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/')
+@app.route('/cities')
 def ShowCities():
 	return "This Page will list all the cities"
 
+@app.route('/city/new')
+def newCity():
+	return "This Page will be for making a new city"
+
+@app.route('/city/<int:city_id>/edit')
+def editCity(city_id):
+	return "This Page will be for editing city %s" % city_id
+
+@app.route('/city/<int:city_id>/delete')
+def deleteCity(city_id):
+	return "This Page will be for deleting city %s" % city_id
+
+@app.route('/city/<int:city_id>/')
+@app.route('/city/<int:city_id>/places')
+def ShowPlaces(city_id):
+	return "This Page will list all the the places for the city %s" % city_id
+
+@app.route('/city/<int:city_id>/place/new')
+def newPlace(city_id):
+	return "This Page will be for making a new place for city %s" % city_id
+
+@app.route('/city/<int:city_id>/<int:place_id>/edit')
+def eidtPlace(city_id, place_id):
+	return "This Page will be for editing place %s" % place_id
+
+@app.route('/city/<int:city_id>/<int:place_id>/delete')
+def deletePlace(city_id, place_id):
+	return "This Page will be for deleting place %s" % place_id
 
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=7000)
--- database_setup.py
+++ database_setup.py
@@ -3,4 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+engine = create_engine('sqlite:///citiescatalog.db')
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+Base.metadata.create_all(engine)
