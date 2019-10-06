60642eefc31ae83d5e6f1b1410a1f91a2dd33af8
AsemDevs <asemhamdi97@gmail.com>
2019-10-06 22:21:56 +0300

    Fix 2

--- application.py
+++ application.py
@@ -114,6 +114,11 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
